b6d2f978a15ec8a5be1ca572bf0ab2ce69fa16cf
SengMing Tan <tansengming@gmail.com>
2017-10-26 16:50:57 -0400

    adds ex admin to mix

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Hello.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:ex_admin, github: "smpallen99/ex_admin", branch: "phx-1.3"},
       {:phoenix, "~> 1.3.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
